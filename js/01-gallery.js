7a267307e7052214cfac8107d8f507f0b3d7e195
DimaKrupenko <dimakrupenko77@gmail.com>
2022-10-14 19:21:56 +0300

    сделал первое задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const itemEl = galleryItems
+  .map(
+    (item) =>
+      //   `<div class="gallery__item">
+      //         <a class="gallery__link" href=${item.original} onclick="return false;">
+      //         <img class="gallery__image" src=${item.preview} data-source=${item.original} alt='${item.description}'></a>
+      //     </div> `
+      `<div class="gallery__item">
+    <a class="gallery__link" href=${item.original} onclick="return false;">
+    <img class="gallery__image" src=${item.preview} alt='${item.description}' title='${item.description}'/></a>
+    
+</div>
+          `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", itemEl);
+
+import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.esm";
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
