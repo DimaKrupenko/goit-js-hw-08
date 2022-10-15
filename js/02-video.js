6d566a810fd7d3c7a2a49da4bc73b546ba7f7e5f
DimaKrupenko <dimakrupenko77@gmail.com>
2022-10-15 17:50:09 +0300

    работаю со второй задачей

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,19 @@
+import Player from "@vimeo/player";
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+player.on("play", function (event) {
+  console.log("played the video!", event);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log("title:", title);
+});
+
+player.on("pause", function (event) {
+  const sec = event.seconds;
+  console.log(sec, event);
+});
+
+console.log(sec);
